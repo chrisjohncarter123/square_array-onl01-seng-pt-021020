b742c1d3127d25b5aad46a98a85a6da0b4a53b4b
Christopher Carter <chrisjohncarter123@gmail.com>
2020-02-16 04:55:28 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,5 @@
 def square_array(array)
-  result = Array.new(array.length)
-  counter = 0
-  array.each do | num | 
-    result[counter] = num * num
-    counter += 1
-  end
-  result
+  
+  array.collect {|x| x * x }
+  
 end
